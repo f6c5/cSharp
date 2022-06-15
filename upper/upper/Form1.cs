906a39ed4508a79311a0c0f6699cc8842744d447
ferhat <84874779+f6c5@users.noreply.github.com>
2022-06-15 14:26:41 +0300

    csharp

---
+++ upper/upper/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void çevir_Click(object sender, EventArgs e)
+        {
+            buyuk.Text = kucuk.Text.ToUpper();
+        }
+
+        private void degis_Click(object sender, EventArgs e)
+        {
+            kucuk.Text = kucuk.Text.Replace(hedef.Text, kelime.Text);
+            buyuk.Text = (kucuk.Text.ToUpper()).Replace(hedef.Text, kelime.Text);
+        }
+    }
+}
