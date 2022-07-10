92b85ba0f08d0dbf44f252c91d12c550cafe361a
ferhat <84874779+f6c5@users.noreply.github.com>
2022-07-10 18:15:38 +0300

    cSharp

---
+++ wonkaApp/wonka/wonka/cs_data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wonka
+{
+    class cs_data
+    {
+        public static string path = "Data Source=.;Initial Catalog=wonka;Integrated Security=True";
+    }
+}
+
