fa164ff9eec616cb6861f49f9267ffb3e96d631e
ferhat <84874779+f6c5@users.noreply.github.com>
2022-07-10 17:50:03 +0300

    cSharp

---
+++ upper/upper/Form1.cs
@@ -0,0 +1,65 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void çevir_Click(object sender, EventArgs e)
+        {
+            buyuk.Text = kucuk.Text.ToUpper();
+        }
+
+        private void degis_Click(object sender, EventArgs e)
+        {
+            kucuk.Text = kucuk.Text.Replace(hedef.Text, kelime.Text);
+            buyuk.Text = (kucuk.Text.ToUpper()).Replace(hedef.Text, kelime.Text);
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void çevir_Click(object sender, EventArgs e)
+        {
+            buyuk.Text = kucuk.Text.ToUpper();
+        }
+
+        private void degis_Click(object sender, EventArgs e)
+        {
+            kucuk.Text = kucuk.Text.Replace(hedef.Text, kelime.Text);
+            buyuk.Text = (kucuk.Text.ToUpper()).Replace(hedef.Text, kelime.Text);
+        }
+    }
+}
+>>>>>>> 906a39ed4508a79311a0c0f6699cc8842744d447
---
+++ upper/upper/Program.cs
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+>>>>>>> 906a39ed4508a79311a0c0f6699cc8842744d447
